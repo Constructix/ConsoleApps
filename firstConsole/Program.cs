526752ea754772d32361fd22afc477d66c8d6fd8
R Jones <rjjones79@hotmail.com>
2025-11-16 09:20:27 +1000

    Initial Commit to git

---
+++ firstConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Enter First Reading: ");
+var firstReadingValid = int.TryParse(Console.ReadLine(), out int firstReading) ;
+Console.WriteLine();
+Console.Write("Enter Second Reading: ");
+var secondReadingValid = int .TryParse( Console.ReadLine(), out int secondReading);
+var result = secondReading - firstReading;
+Console.WriteLine($"Usage: {result}");
+
+
